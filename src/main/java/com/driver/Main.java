d09af5855a5c080b914c2697f3b0c0f635850b1c
shivaprasadsanikop <shivaprasadsanikop19@gmail.com>
2024-03-10 21:17:02 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Main obj = new Main();
         Product obj1 = obj.new Product();
-       obj1.product(2,4);
-       obj1.product(2,4,6);
-       obj1.product(2.0,4.0);
+        System.out.println(obj1.product(2,4));
+        System.out.println(obj1.product(2,4,6));
+        System.out.println(obj1.product(2.0,4.0));
 
     }
       class Product{
